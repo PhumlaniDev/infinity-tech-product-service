493453d8fed5425f251349a581a221ae53009157
Phumlani Arendse <aphumlani.dev@gmail.com>
2025-06-25 23:05:44 +0200

    remove placeholder comments from ProductServiceApplication

--- src/main/java/com/phumlanidev/productservice/ProductServiceApplication.java
+++ src/main/java/com/phumlanidev/productservice/ProductServiceApplication.java
@@ -4,16 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-/**
- * Comment: this is the placeholder for documentation.
- */
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 @SpringBootApplication
 public class ProductServiceApplication {
 
-  /**
-   * Comment: this is the placeholder for documentation.
-   */
   public static void main(String[] args) {
     SpringApplication.run(ProductServiceApplication.class, args);
   }
